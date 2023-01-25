738159d6e55f7893c1a027133a69399b5a567ff8
azdvsbatch3 azdvsbatch3 <azdvsbatch3@outlook.com>
2023-01-25 14:39:59 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -43,3 +43,39 @@ resource "azurerm_network_interface" "javanic" {
     public_ip_address_id          = azurerm_public_ip.javapubip.id
   }
 }
+
+resource "azurerm_virtual_machine" "javavm" {
+  name                = "${var.application_name}-vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.javarg.name
+  network_interface_ids = [azurerm_network_interface.javanic.id]
+  vm_size               = var.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.5"
+    version   = "latest"
+  }
+  
+  storage_os_disk {
+    name                = "${var.application_name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.application_name}-vm"
+    admin_username = var.vm_username
+    admin_password = var.vm_pass
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
