78dc0b9d62d2780bfa95d7a973163faa411f7e19
azdvsbatch3 azdvsbatch3 <azdvsbatch3@outlook.com>
2023-01-27 03:22:02 +0000

    Updated providers.tf

--- providers.tf
+++ providers.tf
@@ -1,20 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.40.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "dvsterraformiac"
-    container_name       = "statefilebackup"
-    key                  = "default.tfstate"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.40.0"
+    }
   }
 
+    backend "azurerm" {
+        resource_group_name  = "terraform-rg"
+        storage_account_name = "dvsterraformiac"
+        container_name       = "statefilebackup"
+        key                  = "default.tfstate"
+    }
 }
-provider "azurerm" {
-  # Configuration options
-  features {}
+
+provider "azurerm" {
+  # Configuration options
+        features {}
+
+
 }
